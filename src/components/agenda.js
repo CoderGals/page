1e03bc595542d7d7783cc76ee94c7786f9003673
ismajlramadani <helloismajl@gmail.com>
2020-11-29 23:57:44 +0100

    agenda rendered from json

--- src/components/agenda.js
+++ src/components/agenda.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Container, Row, Button, Card} from 'react-bootstrap';
+import agenda, { DEFAULT_SELECTED } from '../data/agenda';
 
 export const Agenda = () => {
+    const [activeDay, setActiveDay] = useState(DEFAULT_SELECTED);
+    const day = agenda[activeDay];
+
     return (
         <div className="agenda-component">
             <Container>
@@ -12,141 +16,41 @@ export const Agenda = () => {
                 </Row>
                 <Row>
                     <div className="agenda-content">
-                        <Button>Day 1 - Friday 11.12.2020</Button>
-                        <Button>Day 2 - Friday 12.12.2020</Button>
-                        <Button>Day 3 - Friday 13.12.2020</Button>
+                        {
+                            Object.keys(agenda).map(key => (
+                                <Button className={key === activeDay ? 'active-agenda': ''} onClick={() => setActiveDay(key) }>{agenda[key].label}</Button>
+                            ))
+                        }
                     </div>
                 </Row>
 
                 <div className="agenda-content-cards">
-                    <Row>
-                        <div className="agenda-content-card-first">
-                            <div className="agenda-content-time">
-                                <p>12:00 - 13:00</p>
-                            </div>
-                            <div className="card-row">
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Opening Ceremony:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Welcoming words by the organizers, and kick off!
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Opening Ceremony:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Welcoming words by the organizers, and kick off!
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Opening Ceremony:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Welcoming words by the organizers, and kick off!
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Opening Ceremony:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Welcoming words by the organizers, and kick off!
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        </div>
-                    </Row>
-
-                    <Row>
-                        <div className="agenda-content-card-second">
-                            <div className="agenda-content-time">
-                                <p>13:00 - 14:00</p>
-                            </div>
-                            <div className="card-row">
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Mentoring:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Mentors will join the teams and support them to get familiar with the
-                                            project.
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Set Up:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Introduction to projects
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        </div>
-                    </Row>
-
-                    <Row>
-                        <div className="agenda-content-card-third">
-                            <div className="agenda-content-time">
-                                <p>12:00 - 13:00</p>
-                            </div>
-                            <div className="card-row">
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Opening Ceremony:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Welcoming words by the organizers, and kick off!
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        </div>
-                    </Row>
-
-                    <Row>
-                        <div className="agenda-content-card-fourth">
-                            <div className="agenda-content-time">
-                                <p>12:00 - 13:00</p>
-                            </div>
-                            <div className="card-row">
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>
-                                            Opening Ceremony:
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Welcoming words by the organizers, and kick off!
-                                        </Card.Text>
-                                        <Button>Read more</Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        </div>
-                    </Row>
+                    {
+                        day.slots.map(slot => (
+                            <Row>
+                                <div className="agenda-content-card-first">
+                                    <div className="agenda-content-time">
+                                        <p>{slot.time}</p>
+                                    </div>
+                                    <div className="card-row">
+                                        {slot.activities.map(activity => (
+                                            <Card>
+                                                <Card.Body>
+                                                    <Card.Title>
+                                                        {activity.title}
+                                                    </Card.Title>
+                                                    <Card.Text>
+                                                        {activity.description}
+                                                    </Card.Text>
+                                                    <Button>Read more</Button>
+                                                </Card.Body>
+                                            </Card>
+                                        ))}
+                                    </div>
+                                </div>
+                            </Row>
+                        ))
+                    }
                 </div>
             </Container>
         </div>
---
+++ src/data/agenda.js
@@ -0,0 +1,101 @@
+const agenda = {
+    'day-1': {
+        'label': 'Day 1 - Friday 11.12.2020 edited',
+        'slots': [
+            {
+                'time': '12: 00 - 13:00',
+                'activities': [
+                    {
+                        'title': 'Opening Ceremony e paaaraaa',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    },
+                    {
+                        'title': 'Opening Ceremony e dyta',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    },
+                    {
+                        'title': 'Opening Ceremony e treta',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    }
+                ]
+            },
+            {
+                'time': '13: 00 - 14:00',
+                'activities': [
+                    {
+                        'title': 'Mentoring',
+                        'description': 'Mentors will join the teams and support them to get familiar with the project.'
+                    },
+                    {
+                        'title': 'Set Up:',
+                        'description': 'Introduction to projects'
+                    }
+                ]
+            },
+            {
+                'time': '12: 00 - 13:00',
+                'activities': [
+                    {
+                        'title': 'Another one #',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    }
+                ]
+            },
+            {
+                'time': '12: 00 - 13:00',
+                'activities': [
+                    {
+                        'title': 'And this one #',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    }
+                ]
+            }
+        ]
+    },
+    'day-2': {
+        'label': 'Day 2 - Saturday 12.12.2020',
+        'slots': [
+            {
+                'time': '12: 00 - 13:00',
+                'activities': [
+                    {
+                        'title': 'Opening Ceremony #day 2',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    },
+                    {
+                        'title': 'Another activity #day 2 12: 00 - 13:00',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    },
+                ]
+            },
+            {
+                'time': '16: 00 - 17:00',
+                'activities': [
+                    {
+                        'title': 'Another activity #day 2 ###16: 00 - 17:00',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    },
+                ]
+            }
+        ]
+    },
+    'day-3': {
+        'label': 'Day 3 - Sunday 13.12.2020',
+        'slots': [
+            {
+                'time': '10: 00 - 13:00',
+                'activities': [
+                    {
+                        'title': 'Opening Ceremony #day 3',
+                        'description': 'Welcoming words by the organizers, and kick off!'
+                    },
+                ]
+            },
+
+        ]
+    }
+};
+export default agenda;
+
+const DEFAULT_SELECTED = 'day-1';
+export { DEFAULT_SELECTED };
