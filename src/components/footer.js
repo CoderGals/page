965cf0f38cd8b41e4489db0c5234eadf9350d6e8
Qendresa Hoti <qendresahhoti@gmail.com>
2020-11-30 01:08:45 +0100

    footer init

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import {Container, Button, Row} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
+import Image from "next/image";
+import FacebookIcon from '../images/facebook-icon';
+import InstagramIcon from '../images/instagram-icon';
 
 export const Footer = () => {
     return (
         <div className="footer-component">
             <Container>
-                <Row>
-
-                </Row>
+                <div><Image src="/assets/logo.png" alt="me" width="86px" height="86px"/></div>
+                <div><p>&copy; 2020 CoderGals. All rights reserved.</p></div>
+                <div className="social-media">
+                        <span>
+                            <FacebookIcon/>
+                        </span>
+                    <span>
+                            <InstagramIcon/>
+                        </span>
+                </div>
             </Container>
         </div>
     )
