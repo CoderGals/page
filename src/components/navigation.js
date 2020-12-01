c9b731813bb2ec3a8b0d6eb04082d44b764077fc
ismajlramadani <helloismajl@gmail.com>
2020-12-01 21:34:21 +0100

    fixes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from 'next/link';
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 
 const Navigation = () => {
     return (
