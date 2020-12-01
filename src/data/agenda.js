0849041fe908d95ea6aed6b2cbe62be5c819e407
Qendresa Hoti <qendresahhoti@gmail.com>
2020-12-01 19:25:43 +0100

    agenda added

--- src/data/agenda.js
+++ src/data/agenda.js
@@ -24,34 +24,58 @@ const agenda = {
                 ]
             },
             {
-                'time': '12:30',
+                'time': '12:30 - 13:00',
                 'activities': [
                     {
                         'title': 'Speaker Session',
                         'description': 'We will enjoy some time hearing from one of our supporters...',
                         'long_description': 'This section will be dedicated to 15 - 20min speech of Agon Avdimetaj on ...'
-                    },
-                    {
-                        'title': 'Set Up:',
-                        'description': 'Introduction to projects'
                     }
                 ]
             },
             {
-                'time': '12: 00 - 13:00',
+                'time': '16:00 - 18:00',
                 'activities': [
                     {
-                        'title': 'Another one #',
-                        'description': 'Welcoming words by the organizers, and kick off!'
+                        'title': 'Team - Mentor Meeting',
+                        'description': 'The teams will have their 1st checkpoint starting now...',
+                        'long_description': 'Mentors will join the teams to support them on setting up the source code, provide more information about the project scope, and other technical issues the participants might have.\n' +
+                            '\n' + '1.0 Set up: Each team '
+                    }
+                    // {
+                    //     'title': 'Set Up:',
+                    //     'description': 'Introduction to projects'
+                    // }
+                ]
+            },
+            {
+                'time': '18:00 - 20',
+                'activities': [
+                    {
+                        'title': 'Start hacking!',
+                        'description': 'Here comes the fun part!',
+                        'long_description': 'Now each group is ready to crush their editors. Happy coding :).\n'
                     }
                 ]
             },
             {
-                'time': '12: 00 - 13:00',
+                'time': '20:00 - 21:00',
                 'activities': [
                     {
-                        'title': 'And this one #',
-                        'description': 'Welcoming words by the organizers, and kick off!'
+                        'title': 'Gaming Time!',
+                        'description': 'Well, the fun part to be continued...',
+                        'long_description': 'We will take some time off and will try to relax, but what will we play? Lets go and find out.\n'
+                    }
+                ]
+            },
+            ,
+            {
+                'time': '21:00 - morning',
+                'activities': [
+                    {
+                        'title': 'Continue hacking!',
+                        'description': 'Lets get back to work once again...',
+                        'long_description': 'You know everybody loves sharing that good prototype worked over the weekend, so lets do this!.\n'
                     }
                 ]
             }
@@ -61,24 +85,76 @@ const agenda = {
         'label': 'Day 2 - Saturday 12.12.2020',
         'slots': [
             {
-                'time': '12: 00 - 13:00',
+                'time': '09:00 - 9:30',
                 'activities': [
                     {
-                        'title': 'Opening Ceremony #day 2',
-                        'description': 'Welcoming words by the organizers, and kick off!'
+                        'title': 'New day checkpoint',
+                        'description': 'Lets elaborate today`s agenda...',
+                        'long_description': 'During the second day the organizers will go through the agenda, and reiterate the expected final submission.\n'
                     },
+                    // {
+                    //     'title': 'Another activity #day 2 12: 00 - 13:00',
+                    //     'description': 'Welcoming words by the organizers, and kick off!'
+                    // },
+                ]
+            },
+            {
+                'time': '09:30 - 11:00',
+                'activities': [
                     {
-                        'title': 'Another activity #day 2 12: 00 - 13:00',
-                        'description': 'Welcoming words by the organizers, and kick off!'
+                        'title': 'Teams meeting with Mentors',
+                        'description': 'First checkpoint for today`s meeting with mentors...',
+                        'long_description':'During this checkpoint the participants will share their progress on the projects with each other as well as the mentors, they will have the chance to discuss the challenges and blocked issues if any, and finally, the execution project plan for the rest of the day.\n'
                     },
                 ]
             },
             {
-                'time': '16: 00 - 17:00',
+                'time': '11:00 - 15:00',
                 'activities': [
                     {
-                        'title': 'Another activity #day 2 ###16: 00 - 17:00',
-                        'description': 'Welcoming words by the organizers, and kick off!'
+                        'title': 'Start hacking!',
+                        'description': 'Here comes the fun part again!',
+                        'long_description': 'Now each group is ready to crush their editors again! Happy coding :).\n'
+                    },
+                ]
+            },
+            {
+                'time': '15:00 - 16:00',
+                'activities': [
+                    {
+                        'title': 'Team - Mentor Meeting',
+                        'description': 'The teams will have their second checkpoint starting now...',
+                        'long_description': 'During the second checkpoint, the participants will be able to share their journey, ask for suggestions, and discuss any technical issue they have.\n'
+                    },
+                ]
+            },
+            {
+                'time': '16:00 - 20:00',
+                'activities': [
+                    {
+                        'title': 'Continue Hacking..',
+                        'description': 'Back to our favorite editor...',
+                        'long_description': 'Yes, you`re doing great. Keep coding :)!.\n'
+                    },
+                ]
+            },
+            {
+                'time': '20:00 - 21:00',
+                'activities': [
+                    {
+                        'title': 'Gaming Time!',
+                        'description': 'Well, the fun part to be continued...',
+                        'long_description': 'We will take some time off and will try to relax, but what will we play? Lets go and find out.\n'
+                    },
+                ]
+            },
+            {
+                'time': '21:00 - Morning',
+                'activities': [
+                    {
+                        'title': 'Keep coding folks!',
+                        'description': '`Hide and seek champion...`',
+                        'long_description': 'Of course, you can hack throughout the night. Go, get them!!'
                     },
                 ]
             }
@@ -88,11 +164,32 @@ const agenda = {
         'label': 'Day 3 - Sunday 13.12.2020',
         'slots': [
             {
-                'time': '10: 00 - 13:00',
+                'time': '9:30 - 11:00',
                 'activities': [
                     {
-                        'title': 'Opening Ceremony #day 3',
-                        'description': 'Welcoming words by the organizers, and kick off!'
+                        'title': 'Team - Mentor meeting',
+                        'description': 'First and final mentor checkpoint for the day before the great pitch...!',
+                        'long_description': ''
+                    },
+                ]
+            },
+            {
+                'time': '11:00 - 14:00',
+                'activities': [
+                    {
+                        'title': 'Submission deadline!',
+                        'description': 'Team preparations for the final pitch...',
+                        'long_description': 'The adrenaline starts now, as everybody is preparing for their final presentation and the prototype they`ve been working on'
+                    },
+                ]
+            },
+            {
+                'time': '14:00 - 16:00',
+                'activities': [
+                    {
+                        'title': 'The final pitch is here!',
+                        'description': 'After all the hard work we`re close to the end..',
+                        'long_description': 'Teams will have 15 to 20min of presentation each. We will view some  '
                     },
                 ]
             },
